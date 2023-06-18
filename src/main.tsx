ee69cf892e65c37a9f778ebb1cbdbd6cbb6fdc25
wpgodforever <997587729@qq.com>
2023-06-18 19:14:26 +0800

    [add]配置路径别名&提示

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+// 初始化样式
 import 'reset-css'
-import './assets/style/global.scss'
+// 全局样式
+import '@/assets/style/global.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // 路径别名配置
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
