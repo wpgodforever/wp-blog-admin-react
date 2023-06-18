248eea94fb6256fe6993fd1cd2c4f6f2df8ae8a4
wpgodforever <997587729@qq.com>
2023-06-18 19:05:56 +0800

    【add】全局scss样式配置

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'reset-css'
+import './assets/style/global.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
