fae8b0451518009f58320f7bbf20809259c7c1fb
wuping <wuping@xinli001.com>
2023-06-20 16:03:47 +0800

    控制台警告修复

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -5,16 +5,17 @@ import { Button, Input, Space } from 'antd';
 
 const login = () => {
     const [passwordVisible, setPasswordVisible] = React.useState(false);
-
     return (
-        <div className={'container' +' '+style.loginContainer}>
-            <div className={style.loginBox +' '+ 'flexCol'}>
+        <div className={'container' + ' ' + style.loginContainer}>
+            <div className={style.loginBox + ' ' + 'flexCol'}>
                 <div className={style.loginTitle}>
                     wp的博客后台管理系统
                 </div>
-                <Input size="large" placeholder="请输入用户名" prefix={<UserOutlined />} />
-                <Input.Password size="large" placeholder="请输入密码" prefix={<PropertySafetyOutlined />}/>
-            </div>    
+                <form action="#">
+                    <Input size="large" placeholder="请输入用户名" prefix={<UserOutlined />} autoComplete='off'/>
+                    <Input.Password size="large" placeholder="请输入密码" prefix={<PropertySafetyOutlined />} autoComplete='off'/>
+                </form>
+            </div>
         </div>
     );
 }
