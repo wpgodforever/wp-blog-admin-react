f0eabfb7b68ea1e9c58ae635231f906d45dfec1a
wuping <wuping@xinli001.com>
2023-06-20 15:34:13 +0800

    登录页静态样式

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,7 +1,20 @@
+import React from 'react';
+import style from './style.module.scss';
+import { UserOutlined, PropertySafetyOutlined } from '@ant-design/icons';
+import { Button, Input, Space } from 'antd';
+
 const login = () => {
+    const [passwordVisible, setPasswordVisible] = React.useState(false);
+
     return (
-        <div>
-            <h1>login</h1>
+        <div className={'container' +' '+style.loginContainer}>
+            <div className={style.loginBox +' '+ 'flexCol'}>
+                <div className={style.loginTitle}>
+                    wp的博客后台管理系统
+                </div>
+                <Input size="large" placeholder="请输入用户名" prefix={<UserOutlined />} />
+                <Input.Password size="large" placeholder="请输入密码" prefix={<PropertySafetyOutlined />}/>
+            </div>    
         </div>
     );
 }
