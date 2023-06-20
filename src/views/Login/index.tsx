0b5cef64a1951c3e37317af4f990be2d689022dd
wuping <wuping@xinli001.com>
2023-06-20 16:30:58 +0800

    登录交互

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -5,6 +5,11 @@ import { Button, Input, Space } from 'antd';
 
 const login = () => {
     const [passwordVisible, setPasswordVisible] = React.useState(false);
+    const [password, setPassword] = React.useState('');
+    const [username, setUsername] = React.useState('');
+    const submit = () => {
+        console.log(username,password)
+    }
     return (
         <div className={'container' + ' ' + style.loginContainer}>
             <div className={style.loginBox + ' ' + 'flexCol'}>
@@ -12,9 +17,17 @@ const login = () => {
                     wp的博客后台管理系统
                 </div>
                 <form action="#">
-                    <Input size="large" placeholder="请输入用户名" prefix={<UserOutlined />} autoComplete='off'/>
-                    <Input.Password size="large" placeholder="请输入密码" prefix={<PropertySafetyOutlined />} autoComplete='off'/>
+                    <Input size="large" placeholder="请输入用户名" prefix={<UserOutlined />} autoComplete='off'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <Input.Password size="large" placeholder="请输入密码" prefix={<PropertySafetyOutlined />} 
+                    style={{marginTop: '10px'}} value={password} 
+                    autoComplete='off' 
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
                 </form>
+                <Button type="primary" block onClick={submit}>登录</Button>
             </div>
         </div>
     );
