1de29f4af32bfaceda275472ae1d54efbd4cc1e1
wuping <wuping@xinli001.com>
2023-11-20 18:21:48 +0800

    [add]已登录的不能访问登录页

--- src/routers/guard.ts
+++ src/routers/guard.ts
@@ -1,14 +1,17 @@
 import { useRoutes, useNavigate, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
 import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 const Guard = (props) => {
-    const { token } = props.loginReducer
+    const { token } = useSelector(state => state.loginReducer)
     const navigate = useNavigate()//useNavigate钩子返回一个函数，这个hooks能够让我可以编程式的导航。
     const location = useLocation();//这个钩子返回当前路由对象。如果您想在当前路由更改时执行一些副作用，我们就可以使用这个hooks。
     useEffect(() => {
         console.log(token, 'token')
         if (!token && location.pathname !== '/login') {
             navigate('/login')
+        }else if(token && location.pathname === '/login'){
+            navigate('/')
         }
     }, [location.pathname])
     const outlet = useRoutes(props.router)
