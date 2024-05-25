3240afed6d750acb93053628da8034926102c36d
wpgodforever <997587729@qq.com>
2024-05-25 22:38:36 +0800

    [add]引入rollup-plugin-copy解决打包问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    copy({
+      targets: [
+        // 将根目录下的sw.ts复制到dist目录下
+        { src: 'sw.ts', dest: 'dist' },
+        
+      ],
+      hook: 'closeBundle',
+    })
+  ],
   resolve: {
     alias: {
       // 路径别名配置
@@ -12,11 +22,12 @@ export default defineConfig({
     },
   },
   build: {
-    // 将根目录下的sw.js打包到dist目录下
+    // 将根目录下的sw.ts打包到dist目录下,而不是assets目录下
+    outDir: 'dist',
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
-        sw: path.resolve(__dirname, 'sw.js'),
+        sw: path.resolve(__dirname, 'sw.ts'),
       },
     },
   }
